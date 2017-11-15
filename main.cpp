d932d2303ecb1d52aee824667c3d7736174a8974
AlanQuille <33432976+AlanQuille@users.noreply.github.com>
2017-11-15 22:58:10 +0000

    Got log uniform and Moore-Penrose to work.
    The file "Eigen" is a portable lightweight easy to use linear algebra library that does not require linking, all it requires is that the include path of the compiler includes Eigen. It does the Moore Penrose pseudoinverse in a very simple easy to understand format. It is working correctly here. I also got the log uniform to work correctly, the main.cpp includes a test for this (the instances of nums between 0 and 10 equals the number between 10 and 100 and 100 and 1000, approximately). Also, this version includes a test if the Delaunay Triangulation fails to connect a node to another node. We want control over how many springs there is ourselves and might want to eliminate some springs/nodes afterwards, but the simulation should start with every node connected to at least one other node.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "Simulation.hpp"
+#include "Eigen/QR"
 using namespace std;
+using namespace Eigen;
 
 
 int main(int argc, char** argv)
@@ -19,15 +21,60 @@ int main(int argc, char** argv)
 	data.range1x = 1;
 	data.range0y = 0;
 	data.range1y = 1;
-	data.initial_log_uniform = 0;
-	data.final_log_uniform = 1;
-	data.initial_uniform = 0;
-	data.final_uniform = 1;
+	data.initial_log_uniform = 1;
+	data.final_log_uniform = 100;
+	data.initial_uniform = 100;
+	data.final_uniform = 200;
   data.t0 = 0;
-	data.tmax = 0.1;
+	data.tmax = 1;
 	data.dt = 0.001;
 
   Simulation sim(data);
+
+  for(int i =0; i<sim.Spring_List(); i++)
+  {
+    cout <<"Weight for spring" <<" " << sim.Spring_Return(i).Output_Weight() << endl;
+  }
+
+//  sim.Springs
+
+
+
+  double test = 0;
+
+  int lessthan10 =0;
+  int lessthan100 =0;
+  int lessthan1000 = 0;
+
+  sim.Log_10_Uniform(1, 100);
+  for(int i=0; i<100000; i++)
+  {
+    test = sim.Log_10_Uniform(0, 1000);
+    if(test <=10) lessthan10++;
+    if(test <=100 && test>10) lessthan100++;
+    if(test <=1000 && test>100) lessthan1000++;
+  }
+  cout <<"If the log to the base 10 uniform distribution is functioning correctly, these numbers should be approx. equal.";
+  cout << endl;
+  cout << lessthan10;
+  cout << endl;
+  cout << lessthan100;
+  cout <<endl;
+  cout << lessthan1000;
+  cout <<endl;
+ cout <<"Test to see if the Moore-Penrose pseudoinverse is functioning correctly.";
+ MatrixXd m(2,2);
+ m(0,0) = 1;
+ m(1,0) = 0;
+ m(0,1) = 1;
+ m(1,1) = 0;
+ cout << endl;
+ cout << m << endl;
+ m = m.completeOrthogonalDecomposition().pseudoInverse();
+ cout << m << endl;
+
+
+
 
   return 0;
 }
